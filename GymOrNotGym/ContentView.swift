7caf1a7a9a3a40e73e07b9237371d0ac6efe9243
Michael Chan <michael-za.chan@ubs.com>
2019-10-10 10:38:41 +0800

    Initial Commit

---
+++ GymOrNotGym/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymOrNotGym
+//
+//  Created by Michael on 10/10/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
